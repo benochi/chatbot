bcd7dd694456a878d4a029aa17d8a9328ded3eb8
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-03-01 19:10:43 -0600

    Update stock_tkinter.py
    fixed single price.

--- stock_tkinter.py
+++ stock_tkinter.py
@@ -1,40 +1,53 @@
 import yfinance as yf
-import matplotlib.pyplot as plt
-from tkinter import *
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
-# Define the function to retrieve the stock data and display the graph
-def get_stock_data():
-    # Get the stock symbol entered by the user
-    symbol = stock_symbol.get()
+def get_stock_price(stock_name):
+    stock = yf.Ticker(stock_name)
+    return stock.info['regularMarketPrice']
 
-    # Use yfinance to retrieve the stock data
-    stock = yf.Ticker(symbol)
-    data = stock.history(period="max")
+def get_stock_history(stock_name):
+    stock = yf.Ticker(stock_name)
+    return stock.history(period="max")
 
-    # Plot the closing price data as a line graph
-    plt.plot(data['Close'])
-    plt.title(symbol)
-    plt.xlabel('Date')
-    plt.ylabel('Price')
+def display_stock_price():
+    stock_name = entry.get()
+    data = yf.Ticker(stock_name)
+    price = data.history(period='1d')['Close'][0]
+    price_text.set(f"Current Price: ${price:.2f}")
 
-    # Display the graph in the Matplotlib window
-    plt.show()
+def display_stock_graph():
+    stock_name = entry.get()
+    history = get_stock_history(stock_name)
+    fig = Figure(figsize=(5, 4), dpi=100)
+    subplot = fig.add_subplot(111)
+    subplot.plot(history.index, history["Close"])
+    subplot.set_title(f"{stock_name} Stock Price History")
+    subplot.set_xlabel("Date")
+    subplot.set_ylabel("Price ($)")
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas.draw()
+    canvas.get_tk_widget().grid(row=3, column=0, columnspan=2)
 
-# Create the main window
-root = Tk()
+root = tk.Tk()
+root.geometry("600x600")
 root.title("Stock Bot")
 
-# Create a label for the stock symbol entry
-symbol_label = Label(root, text="Enter Stock Symbol:")
-symbol_label.pack()
+label = tk.Label(root, text="Enter stock name:")
+label.grid(row=0, column=0)
 
-# Create an entry for the stock symbol
-stock_symbol = Entry(root)
-stock_symbol.pack()
+entry = tk.Entry(root)
+entry.grid(row=0, column=1)
 
-# Create a button to retrieve the stock data and display the graph
-get_data_button = Button(root, text="Get Stock Data", command=get_stock_data)
-get_data_button.pack()
+price_text = tk.StringVar()
+price_label = tk.Label(root, textvariable=price_text)
+price_label.grid(row=1, column=0, columnspan=2)
 
-# Run the main loop
+price_button = tk.Button(root, text="Get Current Price", command=display_stock_price)
+price_button.grid(row=2, column=0)
+
+graph_button = tk.Button(root, text="Display Price History", command=display_stock_graph)
+graph_button.grid(row=2, column=1)
+
 root.mainloop()
