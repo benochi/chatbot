ae86efdcea5cce946e6b975dac1fee56c8696e84
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-03-01 19:04:17 -0600

    updated

--- stock_tkinter.py
+++ stock_tkinter.py
@@ -1,29 +1,30 @@
+import yfinance as yf
 import tkinter as tk
-import requests
 
-class StockBot:
-    def __init__(self, symbol):
-        self.symbol = symbol
-        self.price = tk.StringVar()
-        self.volume = tk.StringVar()
-        self.update_data()
-        
-    def update_data(self):
-        url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={self.symbol}&apikey=YOUR_API_KEY"
-        data = requests.get(url).json()["Global Quote"]
-        self.price.set(data["05. price"])
-        self.volume.set(data["06. volume"])
-        self.window.after(60000, self.update_data)
-        
-    def run(self):
-        self.window = tk.Tk()
-        self.window.title(f"StockBot: {self.symbol}")
-        tk.Label(self.window, text="Price:").grid(row=0, column=0)
-        tk.Label(self.window, textvariable=self.price).grid(row=0, column=1)
-        tk.Label(self.window, text="Volume:").grid(row=1, column=0)
-        tk.Label(self.window, textvariable=self.volume).grid(row=1, column=1)
-        self.window.mainloop()
+def retrieve_data():
+    ticker = e1.get()
+    data = yf.Ticker(ticker)
+    price = data.history(period='1d')['Close'][0]
+    e2.delete(0, tk.END)
+    e2.insert(0, str(price))
 
-if __name__ == "__main__":
-    bot = StockBot("AAPL")
-    bot.run()
+root = tk.Tk()
+root.geometry('300x200')
+root.title('StockBot')
+
+label1 = tk.Label(root, text='Enter ticker symbol:')
+label1.pack()
+
+e1 = tk.Entry(root)
+e1.pack()
+
+button = tk.Button(root, text='Get Price', command=retrieve_data)
+button.pack()
+
+label2 = tk.Label(root, text='Price:')
+label2.pack()
+
+e2 = tk.Entry(root)
+e2.pack()
+
+root.mainloop()
